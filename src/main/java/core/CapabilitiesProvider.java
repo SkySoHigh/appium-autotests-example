c7a298806d45d92f693c24571e7b05a7918040af
hramov <hramov@protei.ru>
2022-04-28 21:17:41 +0300

    Fixed CapabilitiesProvider

---
+++ src/main/java/core/CapabilitiesProvider.java
@@ -0,0 +1,42 @@
+package core;
+
+
+import lombok.Getter;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.io.File;
+
+
+public class CapabilitiesProvider {
+
+    @Getter private static DesiredCapabilities capabilities;
+
+    CapabilitiesProvider() {
+
+        capabilities = new DesiredCapabilities();
+        File app = new File(PropertiesProvider.getDeviceProperties().getAppPath());
+        capabilities.setCapability("platformName", PropertiesProvider.getDeviceProperties().getPlatformSystem());
+        capabilities.setCapability("deviceName", PropertiesProvider.getDeviceProperties().getDeviceName());
+        capabilities.setCapability("app", app.getAbsolutePath());
+
+        // Reset strategies -> https://appium.io/docs/en/writing-running-appium/other/reset-strategies/index.html
+        if ( PropertiesProvider.getDeviceProperties().getFullReset()) {
+            capabilities.setCapability("fullReset", PropertiesProvider.getDeviceProperties().getFullReset());
+        } else {
+            capabilities.setCapability("noReset", true);
+        }
+
+        switch ( PropertiesProvider.getDeviceProperties().getPlatformSystem() ) {
+            case Android:
+                capabilities.setCapability("automationName", "XCUITest");
+                capabilities.setCapability("appPackage", "com.jayway.contacts");
+                break;
+            case iOS:
+                capabilities.setCapability("automationName", "XCUITest");
+                break;
+        }
+
+    }
+}
+
+
