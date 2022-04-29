b3fb4afc5e74d4e7563bbd85ee8295314a2c6d56
SkySoHigh <SkySoHigh39@gmail.com>
2022-04-29 20:39:14 +0300

    Added connection of properties and launch parameters (capabilities) through aggregation

--- src/main/java/core/CapabilitiesProvider.java
+++ src/main/java/core/CapabilitiesProvider.java
@@ -9,32 +9,27 @@ import java.io.File;
 
 public class CapabilitiesProvider {
 
-    @Getter private static DesiredCapabilities capabilities;
+    @Getter private final DesiredCapabilities capabilities;
 
-    CapabilitiesProvider() {
+    CapabilitiesProvider(PropertiesProvider properties) {
 
         capabilities = new DesiredCapabilities();
-        File app = new File(PropertiesProvider.getDeviceProperties().getAppPath());
-        capabilities.setCapability("platformName", PropertiesProvider.getDeviceProperties().getPlatformSystem());
-        capabilities.setCapability("deviceName", PropertiesProvider.getDeviceProperties().getDeviceName());
+        File app = new File(properties.getDeviceProperties().getAppPath());
+        capabilities.setCapability("platformName", properties.getDeviceProperties().getPlatformSystem());
+        capabilities.setCapability("deviceName", properties.getDeviceProperties().getDeviceName());
         capabilities.setCapability("app", app.getAbsolutePath());
 
         // Reset strategies -> https://appium.io/docs/en/writing-running-appium/other/reset-strategies/index.html
-        if ( PropertiesProvider.getDeviceProperties().getFullReset()) {
-            capabilities.setCapability("fullReset", PropertiesProvider.getDeviceProperties().getFullReset());
+        if ( properties.getDeviceProperties().getFullReset()) {
+            capabilities.setCapability("fullReset", properties.getDeviceProperties().getFullReset());
         } else {
             capabilities.setCapability("noReset", true);
         }
 
-        switch ( PropertiesProvider.getDeviceProperties().getPlatformSystem() ) {
-            case Android:
-                capabilities.setCapability("automationName", "UiAutomator2");
-                break;
-            case iOS:
-                capabilities.setCapability("automationName", "XCUITest");
-                break;
+        switch (properties.getDeviceProperties().getPlatformSystem()) {
+            case Android -> capabilities.setCapability("automationName", "UiAutomator2");
+            case iOS -> capabilities.setCapability("automationName", "XCUITest");
         }
-
     }
 }
 
