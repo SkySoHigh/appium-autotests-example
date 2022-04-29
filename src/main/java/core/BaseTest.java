0449146bbcd6860e8470f1350562a11b3ee1f17a
SkySoHigh <SkySoHigh39@gmail.com>
2022-04-29 20:40:15 +0300

    Added BaseTest template (in progress)

--- src/main/java/core/BaseTest.java
+++ src/main/java/core/BaseTest.java
@@ -4,8 +4,21 @@ import java.net.MalformedURLException;
 
 public class BaseTest {
 
+    protected BaseRouter baseRouter;
+    protected PropertiesProvider properties;
+    protected CapabilitiesProvider capabilities;
+
     public BaseTest() throws MalformedURLException {
-        new PropertiesProvider();
-        new CapabilitiesProvider();
+        // TODO test with parallel run and optimize if necessary
+        properties = new PropertiesProvider();
+        capabilities = new CapabilitiesProvider(properties);
+        baseRouter = new BaseRouter();
+
+
+        // DriverProvider share initialized driver across all pages via static getter
+        Driver driver = new Driver(properties, capabilities);
+        new DriverProvider(driver);
+
+
     }
 }
